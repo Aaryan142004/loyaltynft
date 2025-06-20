510f57c6a377d7d5199de295c3995518b80ccb1a
Aaryan Malik <aaryanmalik2004@gmail.com>
2025-06-20 21:41:49 +0530

    fix: corrected Web3 import to use CommonJS syntax

--- server/utils/web3.js
+++ server/utils/web3.js
@@ -1,13 +1,16 @@
-const { Web3 } = require('web3');
+const Web3 = require('web3');
 const fs = require('fs');
 const path = require('path');
 require('dotenv').config({ path: '../../.env' });
 
 const web3 = new Web3(process.env.PROVIDER_URL);
+
 const abi = JSON.parse(
-  fs.readFileSync(path.join(__dirname,'..','contract','abi.json'), 'utf8')
+  fs.readFileSync(path.join(__dirname, '..', 'contract', 'abi.json'), 'utf8')
 );
+
 const contract = new web3.eth.Contract(abi, process.env.CONTRACT_ADDRESS);
+
 const adminAccount = web3.eth.accounts.privateKeyToAccount(process.env.ADMIN_PRIVATE_KEY);
 web3.eth.accounts.wallet.add(adminAccount);
 
